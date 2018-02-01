bc738f78790dc62ef831dfdde04922a12b194769
Pipi24 <347127951@qq.com>
2018-02-01 17:32:16 +0800

    twoSum commit

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        jian_dict={}
+        for i in range(len(nums)):
+            if nums[i] in jian_dict:
+                return [jian_dict[nums[i]],i]
+            else:
+                jian_dict[target-nums[i]] = i
