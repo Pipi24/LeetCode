35bd7d988c9164a25d6038d88080364205fe080a
Pipi24 <347127951@qq.com>
2018-02-02 11:47:41 +0800

    twoSum modified

--- twoSum.py
+++ twoSum.py
@@ -6,9 +6,10 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        jian_dict={}
+        #定义一个差值字典，key为差值，value为index
+        jian_dict = {}
         for i in range(len(nums)):
             if nums[i] in jian_dict:
-                return [jian_dict[nums[i]],i]
+                return [jian_dict[nums[i]], i]
             else:
                 jian_dict[target-nums[i]] = i
