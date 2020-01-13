2f866a0b25d034631367e4a9111d64115ec00a91
Pipi24 <347127951@qq.com>
2020-01-13 17:07:25 +0800

    53. Maximum Subarray

---
+++ c++/53. Maximum Subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), INT_MIN);
+        dp[0]=nums[0];
+        //res这样初始化是为了避免数组只有一个数的情况
+        int res = nums[0];
+        for(int i=1; i<nums.size(); ++i){
+            dp[i] = max(dp[i-1]+nums[i], nums[i]);
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
