a2d4df64d8c2fa398a1b510367a94a7e8159c9aa
Pipi24 <347127951@qq.com>
2020-06-26 22:58:35 +0800

    面试题02.01

---
+++ 剑指offer/面试题 02.01. Remove Duplicate Node LCCI.cpp
@@ -0,0 +1,53 @@
+/*
+两重循环
+时间复杂度：O(N^2)，其中 N 是给定链表中节点的数目。
+空间复杂度：O(1)。
+*/
+class Solution {
+public:
+    ListNode* removeDuplicateNodes(ListNode* head) {
+        ListNode* ob = head;
+        while (ob != nullptr) {
+            ListNode* oc = ob;
+            while (oc->next != nullptr) {
+                if (oc->next->val == ob->val) {
+                    oc->next = oc->next->next;
+                } else {
+                    oc = oc->next;
+                }
+            }
+            ob = ob->next;
+        }
+        return head;
+    }
+};
+
+/*
+哈希表
+时间复杂度：O(N)，其中 N 是给定链表中节点的数目。
+空间复杂度：O(N)。在最坏情况下，给定链表中每个节点都不相同，哈希表中需要存储所有的 N 个值。
+*/
+
+class Solution {
+public:
+    ListNode* removeDuplicateNodes(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        unordered_set<int> occurred = {head->val};
+        ListNode* pos = head;
+        // 枚举前驱节点
+        while (pos->next != nullptr) {
+            // 当前待删除节点
+            ListNode* cur = pos->next;
+            if (!occurred.count(cur->val)) {
+                occurred.insert(cur->val);
+                pos = pos->next;
+            } else {
+                pos->next = pos->next->next;
+            }
+        }
+        pos->next = nullptr;
+        return head;
+    }
+};
