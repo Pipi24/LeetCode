bc1a14c3235177602bd2aa3272212ce4e27098ef
Pipi24 <347127951@qq.com>
2020-07-28 16:11:08 +0800

    104 add bfs

--- leetcode/c++/104. Maximum Depth of Binary Tree.cpp
+++ leetcode/c++/104. Maximum Depth of Binary Tree.cpp
@@ -1,3 +1,8 @@
+/*
+递归
+时间复杂度：O(n)
+空间复杂度：O(height)
+*/
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -13,5 +18,38 @@ public:
         if(root==nullptr)
             return 0;
         return max(maxDepth(root->left), maxDepth(root->right))+1;
+    }
+};
+
+/*
+广度优先搜索
+
+广度优先搜索的队列里存放的是「当前层的所有节点」。每次拓展下一层的时候，不同于广度优先搜索的每次只从队列里拿出一个节点，
+我们需要将队列里的所有节点都拿出来进行拓展，这样能保证每次拓展完的时候队列里存放的是当前层的所有节点，
+即我们是一层一层地进行拓展，最后我们用一个变量 ans 来维护拓展的次数，该二叉树的最大深度即为 ans。
+
+时间复杂度：O(n)
+空间复杂度：最坏O(n)
+*/
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        queue<TreeNode*> Q;
+        Q.push(root);
+        int ans = 0;
+        while (!Q.empty()) {
+            int sz = Q.size();
+            while (sz > 0) {
+                TreeNode* node = Q.front();
+                Q.pop();
+                if (node->left) Q.push(node->left);
+                if (node->right) Q.push(node->right);
+                sz -= 1;
+            }
+            ans += 1;
+        } 
+        return ans;
     }
 };
