e0a3d955b2f1d112bc197e0da340774e98e6af61
Pipi24 <347127951@qq.com>
2020-09-10 18:44:03 +0800

    39

--- leetcode/c++/39. Combination Sum.cpp
+++ leetcode/c++/39. Combination Sum.cpp
@@ -26,3 +26,36 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+private:
+    vector<vector<int> > ret;
+    vector<int> temp;
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        dfs(candidates, target, 0, 0);
+        return ret;
+    }
+    void dfs(vector<int>& candidates, int target, int pre_sum, int cur){
+        if(cur == candidates.size())
+            return;
+        
+
+        //等于target，满足条件时
+        if(pre_sum == target){
+            ret.push_back(temp);
+            return;
+        }
+        //选当前值
+        if(candidates[cur]+pre_sum <= target){
+            
+            temp.push_back(candidates[cur]);
+            dfs(candidates, target, pre_sum+candidates[cur], cur);
+            temp.pop_back();
+            
+        }
+        //不选当前值
+        dfs(candidates, target, pre_sum, cur+1);     
+    }
+};
