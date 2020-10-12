9f83df24a759ff0a904c3591db241ac3708c2864
Pipi24 <347127951@qq.com>
2020-10-12 20:57:32 +0800

    416

---
+++ leetcode/c++/416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (auto& num : nums) {
+            sum += num;
+            maxNum = max(maxNum, num);
+        }
+        if (sum & 1) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        vector<int> dp(target + 1, 0);
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            for (int j = target; j >= num; --j) {
+                dp[j] |= dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/partition-equal-subset-sum/solution/fen-ge-deng-he-zi-ji-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
