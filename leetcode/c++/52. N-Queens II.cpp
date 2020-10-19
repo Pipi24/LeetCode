47a826f41c8254913676e7842e616f36b8b78e49
Pipi24 <347127951@qq.com>
2020-10-19 21:37:07 +0800

    52

---
+++ leetcode/c++/52. N-Queens II.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        unordered_set<int> columns, diagonals1, diagonals2;
+        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    int backtrack(int n, int row, unordered_set<int>& columns, unordered_set<int>& diagonals1, unordered_set<int>& diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (columns.find(i) != columns.end()) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.find(diagonal1) != diagonals1.end()) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.find(diagonal2) != diagonals2.end()) {
+                    continue;
+                }
+                columns.insert(i);
+                diagonals1.insert(diagonal1);
+                diagonals2.insert(diagonal2);
+                count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
+                columns.erase(i);
+                diagonals1.erase(diagonal1);
+                diagonals2.erase(diagonal2);
+            }
+            return count;
+        }
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/n-queens-ii/solution/nhuang-hou-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
