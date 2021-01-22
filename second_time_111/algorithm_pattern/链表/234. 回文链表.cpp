a71af61fed71d77f1aff16ab8c2b8be25ddafcc5
Pipi24 <347127951@qq.com>
2021-01-22 19:31:44 +0800

    234. 回文链表

---
+++ second_time_111/algorithm_pattern/链表/234. 回文链表.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/*
+时间复杂度：O(N)
+空间复杂度：O(1)
+*/
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr) {
+            return true;
+        }
+
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode* firstHalfEnd = endOfFirstHalf(head);
+        ListNode* secondHalfStart = reverseList(firstHalfEnd->next);
+
+        // 判断是否回文
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalfStart;
+        bool result = true;
+        while (p1 && p2) {
+            if (p1->val != p2->val) {
+                // 还原链表并返回结果
+                firstHalfEnd->next = reverseList(secondHalfStart);
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }   
+        return true;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    ListNode* endOfFirstHalf(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
