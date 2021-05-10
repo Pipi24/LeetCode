ee7577cb8f3aded250231e264678da2f1538fa8b
Pipi24 <347127951@qq.com>
2021-05-10 23:12:00 +0800

    binary tree

---
+++ second_time_111/二叉树/108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* traversal(vector<int>& nums, int left, int right) {
+        if (left > right) return nullptr;
+        int mid = left + ((right - left) / 2); 
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = traversal(nums, left, mid - 1);
+        root->right = traversal(nums, mid + 1, right);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        TreeNode* root = traversal(nums, 0, nums.size() - 1);
+        return root;
+    }
+};
---
+++ second_time_111/二叉树/77. 组合.cpp
@@ -0,0 +1,53 @@
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) { // 优化的地方
+            path.push_back(i); // 处理节点
+            backtracking(n, k, i + 1);
+            path.pop_back(); // 回溯，撤销处理的节点
+        }
+    }
+public:
+
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return result;
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<int> temp;
+    vector<vector<int>> ans;
+
+    void dfs(int cur, int n, int k) {
+        // 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
+        if (temp.size() + (n - cur + 1) < k) {
+            return;
+        }
+        // 记录合法的答案
+        if (temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+        // 考虑选择当前位置
+        temp.push_back(cur);
+        dfs(cur + 1, n, k);
+        temp.pop_back();
+        // 考虑不选择当前位置
+        dfs(cur + 1, n, k);
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        dfs(1, n, k);
+        return ans;
+    }
+};
